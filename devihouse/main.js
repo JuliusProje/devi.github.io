2a5862882b263e973daea623d4105d74d5b2af9d
Saanshiki <technojiuc@gmail.com>
2020-11-08 01:56:30 +0200

    style.css
    Исправил отступы, поправил отображение иконок.

--- devihouse/main.js
+++ devihouse/main.js
@@ -1,75 +1,75 @@
 $("#signup").click(function() {
-$("#first").fadeOut("fast", function() {
-$("#second").fadeIn("fast");
-});
+    $("#first").fadeOut("fast", function() {
+        $("#second").fadeIn("fast");
+    });
 });
 
 $("#signin").click(function() {
-$("#second").fadeOut("fast", function() {
-$("#first").fadeIn("fast");
-});
+    $("#second").fadeOut("fast", function() {
+        $("#first").fadeIn("fast");
+    });
 });
 
-
-  
-         $(function() {
-           $("form[name='login']").validate({
-             rules: {
-               
-               email: {
-                 required: true,
-                 email: true
-               },
-               password: {
-                 required: true,
-                 
-               }
-             },
-              messages: {
-               email: "S'il vous plaît, mettez une adresse email valide",
-              
-               password: {
-                 required: "Veuillez entrer le mot de passe",
-                
-               }
-               
-             },
-             submitHandler: function(form) {
-               form.submit();
-             }
-           });
-         });
-         
 
 
 $(function() {
-  
-  $("form[name='registration']").validate({
-    rules: {
-      firstname: "required",
-      lastname: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      password: {
-        required: true,
-        minlength: 6
-      }
-    },
-    
-    messages: {
-      firstname: "Entrez votre prénom s'il vous plait",
-      lastname: "Veuillez entrer votre nom de famille",
-      password: {
-        required: "Veuillez fournir un mot de passe",
-        minlength: "Votre mot de passe doit contenir au moins 6 caractères"
-      },
-      email: "S'il vous plaît, mettez une adresse email valide"
-    },
-  
-    submitHandler: function(form) {
-      form.submit();
-    }
-  });
+    $("form[name='login']").validate({
+        rules: {
+
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+
+            }
+        },
+        messages: {
+            email: "S'il vous plaît, mettez une adresse email valide",
+
+            password: {
+                required: "Veuillez entrer le mot de passe",
+
+            }
+
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
+
+
+
+$(function() {
+
+    $("form[name='registration']").validate({
+        rules: {
+            firstname: "required",
+            lastname: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 6
+            }
+        },
+
+        messages: {
+            firstname: "Entrez votre prénom s'il vous plait",
+            lastname: "Veuillez entrer votre nom de famille",
+            password: {
+                required: "Veuillez fournir un mot de passe",
+                minlength: "Votre mot de passe doit contenir au moins 6 caractères"
+            },
+            email: "S'il vous plaît, mettez une adresse email valide"
+        },
+
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
 });
